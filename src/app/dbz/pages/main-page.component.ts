95df3162dfb3413734c500ac780ff66feae49ff4
Lestrades <lestrades@gmail.com>
2024-06-24 19:14:34 +0200

    Clase 61

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.components.html'
+})
+
+export class MainPageComponent  {
+
+
+
+}
